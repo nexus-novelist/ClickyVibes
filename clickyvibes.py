88cfc899eaf9f292ffaed2285f53b52abfcd7a43
ProNoice <robex.omar@gmail.com>
2024-03-15 15:29:21 +0200

    Added keyboard sounds

---
+++ clickyvibes.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import random
+import threading
+
+from pynput import mouse, keyboard
+import pygame
+
+if __name__ == '__main__':
+    pygame.mixer.init()
+    key_folder_path = sys.argv[1]
+    key_volume = int(sys.argv[2]) or 50
+    mouse_folder_path = sys.argv[3]
+    mouse_volume = int(sys.argv[4]) or 50
+    loaded_sounds = {}
+
+    for file in os.listdir(key_folder_path):
+        if (file.endswith('.mp4') or file.endswith('.wav')) and not file.lower().startswith('full'):
+            loaded_sounds.update({file: key_folder_path+'\\'+file})
+    
+    print(loaded_sounds)
+
+    def on_click(x, y, button, pressed):
+        if pressed:
+            print(button)
+
+    def on_type(key):
+        if getattr(key, 'char', 'none') != 'none':
+            if key.char in loaded_sounds:
+                sound = pygame.mixer.Sound(loaded_sounds[key.char])
+                sound.play()
+                sound.set_volume(key_volume/100)
+            else:
+                sound = pygame.mixer.Sound(random.choice(list(loaded_sounds.items()))[1])
+                sound.play()
+                sound.set_volume(key_volume/100)
+        else:
+            sound = pygame.mixer.Sound(random.choice(list(loaded_sounds.items()))[1])
+            sound.play()
+            sound.set_volume(key_volume/100)
+    
+    def start_keyboard_listener():
+        with keyboard.Listener(on_press=on_type) as listener:
+            listener.join()
+    
+    def start_mouse_listener():
+        with mouse.Listener(on_click=on_click) as listener:
+            try:
+                listener.join()
+            except Exception as e:
+                print('{0} was clicked'.format(e.args[0]))
+
+    mouse_listener = threading.Thread(target=start_mouse_listener)
+    keyboard_listener = threading.Thread(target=start_keyboard_listener)
+
+    mouse_listener.start()
+    keyboard_listener.start()
+
+    mouse_listener.join()
+    keyboard_listener.join()
